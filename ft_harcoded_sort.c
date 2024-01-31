662a567f2e317b0d796c593f5a620c6bca4a285d
Sergio Marin Alvarez <smarin-a@c2r10s6.42madrid.com>
2024-01-31 20:53:12 +0100

    compila y he hardocdeado hasta size 2, en proceso de size 3

--- ft_harcoded_sort.c
+++ ft_harcoded_sort.c
@@ -6,7 +6,7 @@
 /*   By: smarin-a <smarin-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 19:02:39 by smarin-a          #+#    #+#             */
-/*   Updated: 2024/01/31 20:49:27 by smarin-a         ###   ########.fr       */
+/*   Updated: 2024/01/31 20:52:22 by smarin-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ static t_list	ft_harcoded_size_two(t_list *stack_a)
 
 static t_list	ft_harcoded_size_three(t_list *stack_a, t_list *stack_b)
 {
+	stack_b = NULL;
 	// Si el numero mas bajo esta en la primera posicion llamar a ft_push(&stack_a, &stack_b)
 	// Si el numero mas bajo esta en la segunda posicion llamar a ft_swap(&stack_a) y despues a ft_push(&stack_a, &stack_b)
 	// Si el numero mas bajo esta en la tercera posicion llamar a ft_rotate_down(&stack_a) y despues a ft_push(&stack_a, &stack_b)
@@ -32,16 +33,19 @@ static t_list	ft_harcoded_size_three(t_list *stack_a, t_list *stack_b)
 
 static t_list	ft_harcoded_size_four(t_list *stack_a, t_list *stack_b)
 {
+	stack_b = NULL;
 	return (*stack_a);
 }
 
 static t_list	ft_harcoded_size_five(t_list *stack_a, t_list *stack_b)
 {
+	stack_b = NULL;
 	return (*stack_a);
 }
 
 t_list	ft_harcoded(t_list **stack_a, t_list **stack_b, int lst_size)
 {
+	stack_a = NULL;
 	if (lst_size == 2)
 		return (ft_harcoded_size_two(*stack_a));
 	else if (lst_size == 3)
@@ -50,4 +54,5 @@ t_list	ft_harcoded(t_list **stack_a, t_list **stack_b, int lst_size)
 		return (ft_harcoded_size_four(*stack_a, *stack_b));
 	else if (lst_size == 5)
 		return (ft_harcoded_size_five(*stack_a, *stack_b));
+	return (**stack_a);
 }
